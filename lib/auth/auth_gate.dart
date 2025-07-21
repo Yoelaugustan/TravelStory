2efb27bc8cb2ae3e6318c67abc73a62a80488989
Yoel Augustan <yoelagustan@gmail.com>
2025-07-21 14:21:50 +0700

    Create Authentication Functions and Listener

---
+++ lib/auth/auth_gate.dart
@@ -0,0 +1,36 @@
+// This Will continuously listen for auth state changes.
+
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:travel_story/pages/login_page.dart';
+import 'package:travel_story/pages/main_page.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      // Listen to auth state changes
+      stream: Supabase.instance.client.auth.onAuthStateChange, 
+      
+      // Bulding the appropriate page based on auth state
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        // check if there is a valid session currently
+        final session = snapshot.hasData ? snapshot.data!.session : null;
+        if (session != null) {
+          return const MainPage();
+        }
+        else {
+          return const LoginPage();
+        }
+      }
+    );
+  }
+}
